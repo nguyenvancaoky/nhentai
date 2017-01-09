33a11d5d9edcf121e162fb64dee5fa5591187ab1
4cat <4catcode@gmail.com>
2017-01-09 15:42:02 +0800

    PEP8

--- nhentai.py
+++ nhentai.py
@@ -6,20 +6,22 @@ import time
 
 
 @click.command()
-@click.option('--pdf', is_flag=True)
-@click.option('--path', help='PATH')
-@click.option('--threads', default=5, help='THREADS')
-@click.option('--id', type=int, prompt='nhentai id',
-              help='ID')
-def main(id, threads, path, pdf):
+@click.option('--id', '-i', type=int, prompt='nhentai id',
+              help='if the link is https://nhentai.net/g/90074/, then the id is 90074')
+@click.option('--needpdf', '-n', is_flag=True, help='Do you need to pack these pictures into a pdf file?')
+@click.option('--path', '-p',
+              help="Where do you want to keep it? If you do not specify a location, it will create a new folder in the current folder, and save the files in it.")
+@click.option('--threads', '-t', default=5, type=click.IntRange(1, 20))
+def main(id, threads, path, needpdf):
     """nhentai downloader"""
     pictures_id, total_pages, pictures_name = get_pictures_info(id)
     path = get_save_path(path, id)
     task = PictureDownloader(pictures_id, total_pages,
                              threads, path)
     task.run()
-    if pdf:
+    if needpdf:
         make_pdf(path, total_pages, pictures_name)
+
 
 if __name__ == '__main__':
     main()
